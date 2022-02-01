85f48502218496fdd1fcf44834c036c7aa011de7
atharvaagate <68295988+atharvaagate@users.noreply.github.com>
2022-02-01 23:43:58 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+def add (x,y) :
+    return x + y
+
+#sub implementationnn
+def sub(x,y) :
+    return x-y
+#mul implementationnn
+def mul(x,y) :
+    return x*y
+#div implementationnn
+def div(x,y) :      ##on master
+    if y==0 :
+        return dividebyzeroerror
+    else :
+        return x/y
+#square implemenatiton
+def square(x) :
+    return x*x
